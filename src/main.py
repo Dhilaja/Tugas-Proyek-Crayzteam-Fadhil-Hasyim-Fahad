487d2809c4ac32de312892d55f0154d6cdc50e5c
Fadhil <fadhil@smkitihsanulfikri.sch.id>
2026-02-19 11:26:27 +0700

    Menambahkan validasi input angka

--- src/main.py
+++ src/main.py
@@ -5,10 +5,13 @@ def main():
     print("=== SISTEM INFORMASI KAS KELAS - CrazyTeam ===")
 
     namasiswa = input("Masukan Nama siswa: ")
+try:
     kasmasuk = int(input("Masukan kas masuk: "))
     kaskeluar = int(input("Masukan kas keluar: "))
     haritelat = int(input("Masukan jumlah hari telat: "))
-
+except ValueError:
+    print(("Input harus berupa angka!"))
+    return
     denda = hitung_denda(haritelat)
     saldo = hitung_saldo(kasmasuk, kaskeluar, denda)
 
