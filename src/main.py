6316fe09ea04ffc83a72e18b5640ca23a9d7d4d5
Fadhil <fadhil@smkitihsanulfikri.sch.id>
2026-02-19 13:40:29 +0700

    aku update main.py nya

--- src/main.py
+++ src/main.py
@@ -5,20 +5,23 @@ def main():
     print("=== SISTEM INFORMASI KAS KELAS - CrazyTeam ===")
 
     namasiswa = input("Masukan Nama siswa: ")
-try:
-    kasmasuk = int(input("Masukan kas masuk: "))
-    kaskeluar = int(input("Masukan kas keluar: "))
-    haritelat = int(input("Masukan jumlah hari telat: "))
-except ValueError:
-    print(("Input harus berupa angka!"))
-    return
-# Proses perhitungan denda dan saldo
+
+    try:
+        kasmasuk = int(input("Masukan kas masuk: "))
+        kaskeluar = int(input("Masukan kas keluar: "))
+        haritelat = int(input("Masukan jumlah hari telat: "))
+    except ValueError:
+        print("Input harus berupa angka!")
+        return
+
+    # Proses perhitungan denda dan saldo
     denda = hitung_denda(haritelat)
     saldo = hitung_saldo(kasmasuk, kaskeluar, denda)
 
     print("Denda:", denda)
     print("Saldo akhir:", saldo)
 
+    # Simpan data transaksi ke file
     simpan_data(namasiswa, kasmasuk, kaskeluar, denda, saldo)
 
 if __name__ == "__main__":
