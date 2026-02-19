7ae1cb7f35b58d646901701eea521842d1153301
fahadbinsyarif <fahadsyarif29@gmail.com>
2026-02-19 07:03:56 +0700

    Menambahkan validasi input pada perhitungan saldo

--- src/kas_logic.py
+++ src/kas_logic.py
@@ -6,3 +6,8 @@ def hitung_denda(haritelat):
     
 def hitung_saldo(kasmasuk, kaskeluar, denda):
     return kasmasuk - kaskeluar - denda
+
+def hitung_saldo(kasmasuk, kaskeluar, denda):
+    if kasmasuk < 0 or kaskeluar < 0:
+        return 0
+    return kasmasuk - kaskeluar - denda
