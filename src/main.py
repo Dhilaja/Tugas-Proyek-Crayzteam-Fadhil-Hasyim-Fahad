193476c6b53ce031dfca06e519f18e2f3c38265e
Fadhil <fadhil@smkitihsanulfikri.sch.id>
2026-02-20 10:30:45 +0700

    update main.py

--- src/main.py
+++ src/main.py
@@ -1,28 +1,53 @@
 from kas_logic import hitung_denda, hitung_saldo
-from data_manager import simpan_data
+from data_manager import *
 
-def main():
-    print("=== SISTEM INFORMASI KAS KELAS - CrazyTeam ===")
+def menu():
+    while True:
+        print("\n=== SISTEM KAS KELAS ===")
+        print("1. Tambah Siswa")
+        print("2. Catat Pembayaran")
+        print("3. Tambah Pengeluaran")
+        print("4. Lihat Siswa Belum Bayar")
+        print("5. Lihat Laporan Kas")
+        print("6. Keluar")
 
-    namasiswa = input("Masukan Nama siswa: ")
+        pilihan = input("Pilih menu: ")
 
-    try:
-        kasmasuk = int(input("Masukan kas masuk: "))
-        kaskeluar = int(input("Masukan kas keluar: "))
-        haritelat = int(input("Masukan jumlah hari telat: "))
-    except ValueError:
-        print("Input harus berupa angka!")
-        return
+        if pilihan == "1":
+            nama = input("Nama siswa: ")
+            tambah_siswa(nama)
+            print("Siswa berhasil ditambahkan!")
 
-    # Proses perhitungan denda dan saldo
-    denda = hitung_denda(haritelat)
-    saldo = hitung_saldo(kasmasuk, kaskeluar, denda)
+        elif pilihan == "2":
+            nama = input("Nama siswa: ")
+            jumlah = int(input("Jumlah bayar: "))
+            hari_telat = int(input("Hari telat: "))
+            denda = hitung_denda(hari_telat)
+            catat_pembayaran(nama, jumlah, denda)
+            print("Pembayaran dicatat. Denda:", denda)
 
-    print("Denda:", denda)
-    print("Saldo akhir:", saldo)
+        elif pilihan == "3":
+            jumlah = int(input("Jumlah pengeluaran: "))
+            tambah_pengeluaran(jumlah)
+            print("Pengeluaran dicatat!")
 
-    # Simpan data transaksi ke file
-    simpan_data(namasiswa, kasmasuk, kaskeluar, denda, saldo)
+        elif pilihan == "4":
+            belum = siswa_belum_bayar()
+            print("Siswa belum bayar:", belum)
+
+        elif pilihan == "5":
+            masuk, keluar = laporan_kas()
+            saldo = hitung_saldo(masuk, keluar)
+            print("Total Masuk:", masuk)
+            print("Total Keluar:", keluar)
+            print("Saldo Akhir:", saldo)
+
+        elif pilihan == "6":
+            print("Terima kasih!")
+            break
+
+        else:
+            print("Pilihan tidak valid!")
 
 if __name__ == "__main__":
-    main()
+    menu()
