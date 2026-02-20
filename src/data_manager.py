594e0de7e7bd63bfc889dc29104bed0b171582a8
Fadhil <fadhil@smkitihsanulfikri.sch.id>
2026-02-20 10:33:07 +0700

    update data_manager.py

--- src/data_manager.py
+++ src/data_manager.py
@@ -1,9 +1,33 @@
-def simpan_data(nama, kasmasuk, kaskeluar, denda, saldo):
-    with open("data_kas.txt", "a", encoding="utf-8") as file:
-        file.write("=== DATA TRANSAKSI KAS KELAS ===\n")
-        file.write(f"Nama Siswa   : {nama}\n")
-        file.write(f"Kas Masuk    : Rp {kasmasuk}\n")
-        file.write(f"Kas Keluar   : Rp {kaskeluar}\n")
-        file.write(f"Denda        : Rp {denda}\n")
-        file.write(f"Saldo Akhir  : Rp {saldo}\n")
-        file.write("-" * 40 + "\n")
+data_siswa = {}
+total_pemasukan = 0
+total_pengeluaran = 0
+
+
+def tambah_siswa(nama):
+    if nama not in data_siswa:
+        data_siswa[nama] = {
+            "bayar": 0,
+            "denda": 0
+        }
+
+
+def catat_pembayaran(nama, jumlah, denda):
+    global total_pemasukan
+
+    if nama in data_siswa:
+        data_siswa[nama]["bayar"] += jumlah
+        data_siswa[nama]["denda"] += denda
+        total_pemasukan += jumlah
+
+
+def tambah_pengeluaran(jumlah):
+    global total_pengeluaran
+    total_pengeluaran += jumlah
+
+
+def siswa_belum_bayar():
+    return [nama for nama, data in data_siswa.items() if data["bayar"] == 0]
+
+
+def laporan_kas():
+    return total_pemasukan, total_pengeluaran
